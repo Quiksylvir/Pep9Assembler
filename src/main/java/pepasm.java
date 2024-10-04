4ce2d7137f20dd51c978bb6039d45678f9326dca
Timothy <quiksylvir@gmail.com>
2024-10-04 14:56:06 -0400

    Add StringBuilder for pep programs

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -1,9 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class pepasm {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         HashMap<String, String> assemblerHashMap = new HashMap<>();
         addMappedValues(assemblerHashMap);
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        Scanner scanner = new Scanner(inputStream);
+        StringBuilder inputStreamStringBuilder = new StringBuilder();
+
+        while (scanner.hasNext()){
+            inputStreamStringBuilder.append
+                            (scanner.next())
+                    .append(" ");
+        }
+        String fileContents = inputStreamStringBuilder.toString();
+        fileContents = fileContents.strip();
+        System.out.println(fileContents);
     }
 
     private static void addMappedValues(HashMap<String, String> assemblerHashMap) {
